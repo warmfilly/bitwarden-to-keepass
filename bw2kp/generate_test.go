40a7531a2eb0ad7e9d59e8817accb972eba92934
Warmfilly <warm.filly@protonmail.com>
2022-11-01 09:17:14 -0600

    Add test file

---
+++ bw2kp/generate_test.go
@@ -0,0 +1,67 @@
+package bw2kp
+
+import (
+	"testing"
+)
+
+const vaultJson = `
+{
+"encrypted": false,
+"folders": [
+	{
+	"id": "100",
+	"name": "Test Folder"
+	}
+],
+"items": [
+	{
+	"id": "200",
+	"organizationId": null,
+	"folderId": "100",
+	"type": 1,
+	"reprompt": 0,
+	"name": "Test Item 1",
+	"notes": "This is a note",
+	"favorite": false,
+	"login": {
+		"username": "username1",
+		"password": "password1",
+		"totp": null
+	},
+	"collectionIds": null
+	},
+	{
+	"id": "201",
+	"organizationId": null,
+	"folderId": null,
+	"type": 1,
+	"reprompt": 0,
+	"name": "Test Item 2",
+	"notes": null,
+	"favorite": false,
+	"login": {
+		"username": "username2",
+		"password": "password2",
+		"totp": null
+	},
+	"collectionIds": null
+	}
+]
+}
+`
+
+func getBitwardenVault() (bitwardenVault, error) {
+	return marshalBitwardenVault([]byte(vaultJson))
+}
+
+func TestCreateKeepassDatabase(t *testing.T) {
+	vault, err := getBitwardenVault()
+
+	if err != nil {
+		t.Fatal("Failed to get Bitwarden Vault")
+	}
+
+	if err = createKeepassDatabase(vault, "../artifacts/db.kdbx", "password"); err != nil {
+		t.Fatal("Failed to create Keepass database")
+	}
+}
